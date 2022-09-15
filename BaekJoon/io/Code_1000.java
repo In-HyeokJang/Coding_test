3e98875b10a32ce4baa52aad739d94f869fe5693
In-HyeokJang <dlsgur1261@naver.com>
2022-09-15 15:50:46 +0900

    codingTest

---
+++ BaekJoon/io/Code_1000.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * 작성일 : 2022.09.15
+ * 내 용 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ */
+
+
+public class Code_1000 {
+    public static void main(String[] args) {
+        System.out.println("A>0, B>0");
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A+B);
+    }
+}
---
+++ BaekJoon/io/Code_10718.java
@@ -0,0 +1,9 @@
+/** 
+    작성일 : 2022.09.15
+    내 용 : 두 줄에 걸쳐 "강한친구 대한육국"을 한 줄에 한 번씩 출력한다.
+ */
+ public class Code_10718{
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+ }
